f78b32cfec02374014c1db0667e5fddd8b00c2bf
Justine Hill <justinehill@Justines-MacBook-Pro.local>
2020-03-24 11:27:35 +0100

    feat:button and app component updates.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,58 @@ import Button from "./components/Button/Button";
 
 function App() {
   return (
-    <>
-      <Button />
-    </>
+    <header>
+      <div className="button_header_wrapper">
+        <h1>Buttons</h1>
+        <p>
+          Custom button styles for actions in forms, dialogs, and more with
+          support for multiple sizes, states, and more.
+        </p>
+        <h2>Examples</h2>
+        <p>
+          Use any of the available button style types to quickly create a styled
+          button. Just modify the variant prop.
+        </p>
+
+        <div className="buttons_container">
+          <Button color="primary">Primary</Button>
+          <Button color="secondary">Secondary</Button>
+          <Button color="success">Success</Button>
+          <Button color="warning">Warning</Button>
+          <Button color="danger">Danger</Button>
+          <Button color="link" isLink hrefLink="/">
+            Link
+          </Button>
+          <Button color="disabled" disabled={true}>
+            Disabled
+          </Button>
+        </div>
+        <pre className="code_box">
+          <code>
+            {`import {Button} from "@hygge-ui/components/Button;
+
+    <Button color="primary">Primary</Button>
+
+    <Button color="secondary">Secondary</Button>
+
+    <Button color="success">Success</Button>
+
+    <Button color="warning">Warning</Button>
+
+    <Button color="danger">Danger</Button>
+
+    <a href="#buttons" color="primary">
+            <button className="link">Link</button>
+    </a>
+
+    <Button color="disabled" disabled>
+            Disabled
+    </Button>
+              `}
+          </code>
+        </pre>
+      </div>
+    </header>
   );
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,91 +2,21 @@ import React from "react";
 import "./Button.css";
 
 const Button = props => {
-  const { disabled, secondary, success, warning, danger, link } = props;
+  const { color, children, disabled, isLink, hrefLink } = props;
 
-  return (
-    <header className="button_header">
-      <div className="button_header_wrapper">
-        <h1>Buttons</h1>
-        <p>
-          Custom button styles for actions in forms, dialogs, and more with
-          support for multiple sizes, states, and more.
-        </p>
-        <h2>Examples</h2>
-        <p>
-          Use any of the available button style types to quickly create a styled
-          button. Just modify the variant prop.
-        </p>
-        <div className="buttons_container">
-          <button>Primary</button>
-
-          <button secondary="secondary">
-            Secondary
-            {secondary}
-          </button>
-
-          <button success="success">
-            Success
-            {success}
-          </button>
-
-          <button warning="warning">
-            Warning
-            {warning}
-          </button>
-
-          <button danger="danger">
-            Danger
-            {danger}
-          </button>
-
-          <button onclick link="link">
-            Link
-            {link}
-          </button>
-
-          <button disabled="disabled">
-            Disabled
-            {disabled}
-          </button>
-        </div>
-        <pre className="code_box">
-          <code>
-            {`import {Button} from "@hygge-ui/components/Button;
-
-  <button>
-    Primary
-  </button>
-
-  <button secondary="secondary">
-    Secondary
-  </button>
-
-  <button success="success">
-    Success
-  </button>
-
-  <button warning="warning">
-    Warning
-  </button>
-
-  <button danger="danger">
-    Danger
-  </button>
-
-  <button link="link">
-    Link
-  </button>
-
-  <button disabled="disabled">
-    Disabled
-  </button>
-              `}
-          </code>
-        </pre>
-      </div>
-    </header>
-  );
+  if (isLink === true) {
+    return (
+      <a href={hrefLink} className={color}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={color} disabled={disabled}>
+        {children}
+      </button>
+    );
+  }
 };
 
 export default Button;
